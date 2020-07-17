822dd1f83974849a9da191caa132fd41e9108f8a
chowa cross <61464091+cforcross@users.noreply.github.com>
2020-07-17 22:47:29 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,7 +146,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 #### USING GMAIL
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ntsendifor@gmail.com'
-EMAIL_HOST_PASSWORD = 'johncarter'
+EMAIL_HOST_USER = 'youremail'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
